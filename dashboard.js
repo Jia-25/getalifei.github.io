fc47847a3c04cab32580cfb2e092a787c9791435
Jia-25 <jiaminrare@gmail.com>
2025-02-22 04:13:55 +0500

    Create dashboard.js

---
+++ dashboard.js
@@ -0,0 +1,6 @@
+// Function to toggle the menu (slide in and slide out)
+function toggleMenu() {
+    const menu = document.getElementById('menu');
+    menu.classList.toggle('open');
+    menu.classList.toggle('closed');
+}
